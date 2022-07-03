dd2376062c174823086310a57a30b9e5c1a2adbe
LedVisell <108343416+LedVisell@users.noreply.github.com>
2022-07-03 23:15:16 +0500

    Add files via upload

---
+++ Program2.cs.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите три числа");  
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int z = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if(x>y && x>z)
+  max = x;
+else if(y>x && y>z) 
+  max = y;
+else if(z>x && z>y)
+  max = z;
+
+Console.Write("Максимальное число=");
+Console.WriteLine(max);
+Console.ReadLine();
