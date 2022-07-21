9a74a643ff31d17c8fc40578c14a7684cee93a49
LedVisell <108343416+LedVisell@users.noreply.github.com>
2022-07-22 01:28:19 +0500

    Add files via upload

---
+++ Les5/36.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите  массив");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+FillArrayRandom(array);
+
+int sum = 0;
+
+for (int i = 1; i < size; i+=2)
+{
+    sum += array[i];
+}
+    Console.WriteLine($" сумма на нечётных позициях = {sum}");
+
+void FillArrayRandom(int[] array)
+{
+    for(int i = 1; i < size; i++)
+        {
+            array[i] = new Random().Next(1,1000);
+        }
+}
---
+++ Les5/38.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите массив");
+
+int size = Convert.ToInt32(Console.ReadLine());
+
+double[] num = new double[size];
+
+double min = 0;
+double max = 0;
+
+FillArrayRandomNum(num);
+
+for (double i = 0; i < size; i++)
+{
+    if (num[i] > max)
+        {
+           max = num[i];
+        }
+    if (num[i] < min)
+        {
+          min = num[i];
+        }
+}
+
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNum(double[] num)
+{
+    for(double i = 0; i < size; i++)
+        {
+            num[i] = new Random().Next();
+        }
+}
