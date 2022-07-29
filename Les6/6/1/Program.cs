27db0cd212db7085da3e5747ec38951b122d8fd3
LedVisell <108343416+LedVisell@users.noreply.github.com>
2022-07-29 22:29:27 +0500

    Add files via upload

---
+++ Les6/6/1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write($"Введи число N(количество чисел): ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] massiveNumbers = new int[n];
+
+void InputNumbers(int n)
+{
+for (int i = 0; i < n; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(n);
+
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
